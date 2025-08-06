090178bdbfdfdff200bf87109fc68669b326956c
Adi Mardani <maniswolfy25@gmail.com>
2025-08-06 10:54:49 +0700

    update

--- resources/views/teknisi/jadwal.blade.php
+++ resources/views/teknisi/jadwal.blade.php
@@ -70,11 +70,12 @@
             <td>
                 @if ($jadwal->status_kehadiran == 'hadir' && !$jadwal->bukti_foto)
                     <form action="{{ route('teknisi.uploadBukti', $jadwal->id) }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="bukti_foto" required>
-    <button type="submit">Tes Upload</button>
-</form>
-
+                        @csrf
+                        <div class="mb-2">  
+                            <input type="file" name="bukti_foto" class="form-control form-control-sm" required>
+                        </div>
+                        <button type="submit" class="btn btn-sm btn-primary">Upload Foto</button>
+                    </form>
                 @elseif ($jadwal->bukti_foto)
                     <a href="{{ asset('storage/bukti_foto/' . $jadwal->bukti_foto) }}" target="_blank" class="btn btn-outline-info btn-sm">
                         Lihat Bukti
