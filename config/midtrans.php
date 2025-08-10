2ccaeceed242650c0ae7be18823615a77787a240
Adi Mardani <maniswolfy25@gmail.com>
2025-08-10 11:28:56 +0700

    update

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,9 +1,8 @@
-    <?php
-
+<?php
 return [
-    'serverKey' => env('MIDTRANS_SERVER_KEY', ''),
-    'clientKey' => env('MIDTRANS_CLIENT_KEY', ''),
-    'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
-    'isSanitized' => env('MIDTRANS_SANITIZED', true),
-    'is3ds' => env('MIDTRANS_IS_3D', true),
+    'server_key'   => env('MIDTRANS_SERVER_KEY', ''),
+    'client_key'   => env('MIDTRANS_CLIENT_KEY', ''),
+    'is_production'=> env('MIDTRANS_IS_PRODUCTION', false),
+    'is_sanitized' => env('MIDTRANS_SANITIZED', true),
+    'is_3ds'       => env('MIDTRANS_IS_3D', true),
 ];
