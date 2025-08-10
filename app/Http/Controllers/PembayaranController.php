bea577f1f450a957581c759a1aa3059a707de72e
Adi Mardani <maniswolfy25@gmail.com>
2025-08-10 12:22:28 +0700

    update

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -19,13 +19,12 @@ public function index($pemesananId)
 {
     $pemesanan = Pemesanan::with('layanan')->findOrFail($pemesananId);
 
-    // Set konfigurasi Midtrans dari config Laravel (ambil dari .env)
-
-Config::$serverKey = config('midtrans.serverKey');
-Config::$clientKey = config('midtrans.clientKey');
-Config::$isProduction = config('midtrans.isProduction');
-Config::$isSanitized = config('midtrans.isSanitized');
-Config::$is3ds = config('midtrans.is3ds');
+    // Set konfigurasi Midtrans dari config Laravel (.env)
+    Config::$serverKey = config('midtrans.server_key');
+    Config::$clientKey = config('midtrans.client_key');
+    Config::$isProduction = config('midtrans.is_production');
+    Config::$isSanitized = config('midtrans.is_sanitized');
+    Config::$is3ds = config('midtrans.is_3ds');
 
     $params = [
         'transaction_details' => [
@@ -40,8 +39,10 @@ Config::$is3ds = config('midtrans.is3ds');
 
     $snapToken = Snap::getSnapToken($params);
 
+    // Pastikan ini yang di return, bukan "true" atau hal lain:
     return view('pembayaran.index', compact('pemesanan', 'snapToken'));
 }
+
 
 
 
